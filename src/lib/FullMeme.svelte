c00214876bfcf50db0e43a476c3e0216ebcf58ef
Alexander Averyanov <alex@averyan.ru>
2022-04-28 21:12:02 +0300

    Meme page update

--- src/lib/FullMeme.svelte
+++ src/lib/FullMeme.svelte
@@ -10,40 +10,23 @@
 	}
 </script>
 
-<div class="meme-container">
-	<div class="meme">
-		<h1 class="meme-title">{meme.name}</h1>
-		<div class="image-container">
-			<img
-				src="{import.meta.env.VITE_API_BASE}/static/images/preview/{meme.id.substring(
-					0,
-					2
-				)}/{meme.id.substring(2, 4)}/{meme.id.substring(4)}.webp"
-				alt={meme.name}
-				class="image"
-			/>
-		</div>
-		<p class="meme-description">{meme.description}</p>
-		<button on:click={remove}>Удалить</button>
+<div class="meme">
+	<h1 class="meme-title">{meme.name}</h1>
+	<div class="image-container">
+		<img
+			src="{import.meta.env.VITE_API_BASE}/static/images/preview/{meme.id.substring(
+				0,
+				2
+			)}/{meme.id.substring(2, 4)}/{meme.id.substring(4)}.webp"
+			alt={meme.name}
+			class="image"
+		/>
 	</div>
+	<p class="meme-description">{meme.description}</p>
+	<button on:click={remove}>Удалить</button>
 </div>
 
 <style>
-	.meme-container {
-		background: rgb(112, 167, 214);
-		background: linear-gradient(60deg, rgba(112, 167, 214, 1) 25%, rgba(43, 167, 162, 1) 72%);
-		border-radius: 5px;
-		border-width: 5px;
-		transition: 0.3s ease-out;
-		cursor: pointer;
-	}
-
-	.meme-container:hover {
-		transform: scale(1.03);
-		/* background-position: 100px; */
-		/* background: linear-gradient(240deg, rgba(112, 167, 214, 1) 25%, rgba(43, 167, 162, 1) 72%); */
-	}
-
 	.meme-title {
 		margin: 1rem;
 	}
